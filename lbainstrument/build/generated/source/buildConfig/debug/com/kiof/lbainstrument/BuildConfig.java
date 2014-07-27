6beb7b61eeedc151940b63b78697a35e9a7f38a3
Kiof <kiof@free.fr>
2014-07-27 17:12:21 +0200

    Initial commit

---
+++ lbainstrument/build/generated/source/buildConfig/debug/com/kiof/lbainstrument/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.kiof.lbainstrument;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "com.kiof.lbainstrument";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 3;
+  public static final String VERSION_NAME = "1.2";
+}
